16ff7d9442d990b23027f1af1cc6e15b4528ba83
Nick Meares <nickmeares@gmail.com>
2022-01-10 16:55:29 +0000

    January 10, 2022 4:55 PM

--- helpers.py
+++ helpers.py
@@ -12,19 +12,21 @@ def dict_search(dictionary: dict, search, list_depth=1):
     dict_search.list_depth = 0
     # Recursive function loops through nesting structure
     def find(d : dict, search):
-        # If dict, check if key matches and append value
+        # If dict, check if key matches and append key-value pair to values list
         if isinstance(d, dict):
             for key, value in d.items():
                 if key in search:
                     temp[key] = value
+                    # if user specified that list depth starts at zero add to values
                     if list_depth == 0:
                         values.append(temp.copy())
-                # If key doesn't match run search on value
+                # If key doesn't match run find on value
                 else:
                     find(value, search)
-        # if list loop through list items
+        # If list, loop through list items
         elif isinstance(d, list):
             for i in range(0,len(d)):
+                # Increment list depth by one to keep track of depth
                 dict_search.list_depth =+ 1
                 find(d[i], search)
                 if dict_search.list_depth == list_depth:

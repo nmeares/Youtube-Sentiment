1b510f75534ee7a9e85471155318de56bb0e2f7b
Nick Meares <nickmeares@gmail.com>
2022-01-17 17:36:07 +0000

    January 17, 2022 5:36 PM

--- update_database.py
+++ update_database.py
@@ -27,19 +27,21 @@ class yt_database():
             cat['region'] = 'GB'
             cat['time_updated'] = dt
 
+        self._insert_or_replace(categories)  
+    
+    def _insert_or_replace(self, dictionary:dict):
+        keys = dictionary.keys()
+        values = [f":{key}" for key in dictionary.keys()]
         try:
             with sqlite3.connect(self.db) as conn:
-                sql = "INSERT OR REPLACE INTO \
-                    categories(category_id, title, assignable, region, time_updated) \
-                    VALUES(:id, :title, :assignable, :region, :time_updated)"
-                conn.executemany(sql, categories)
+                sql = f"INSERT OR REPLACE INTO \
+                    categories({keys}) \
+                    VALUES({values})"
+                conn.executemany(sql, dictionary)
                 print("Categories table updated successfully!")
                 
         except sqlite3.Error as error:
-            print("Error while creating a sqlite table", error)   
-    
-    def _insert_or_replace(self):
-        
+            print("Error while creating a sqlite table", error)
     
     
     

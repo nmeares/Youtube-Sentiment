276ec652f1b03e3fb7c79d317ff59651fbafd488
Nick Meares <nickmeares@gmail.com>
2022-01-17 18:24:42 +0000

    update

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
-from decouple import config
 import sqlite3
 import os
+from decouple import config
 from datetime import datetime
 
 from data_api import youtube
 from helpers import dict_search
 
 
-class yt_database():
+class yt_sentiment():
 
     def __init__(self, db='yt_sentiment.db'):
         self.db = db if os.path.exists(db) else print(f"{db} does not exist! Please create and re-run.")
@@ -26,23 +26,27 @@ class yt_database():
         for cat in categories:
             cat['region'] = 'GB'
             cat['time_updated'] = dt
-
+            # re-name 'id' key to 'categories_id' 
+            cat['category_id'] = cat.pop('id')
+    
         self._insert_or_replace("categories", categories)  
     
-    def _insert_or_replace(self, table, dictionary:dict):
-        keys = dictionary.keys()
-        values = [f":{key}" for key in dictionary.keys()]
+    # Function to insert or replace a list of key value pairs into a sqlite3 database
+    def _insert_or_replace(self, table, value_list:list):
+        # Create key and value lists and sql query string
+        keys = ",".join(list(value_list[0].keys()))
+        values = ",".join([f":{key}" for key in value_list[0].keys()])
+        # Okay to use f-strings in this context as low likelyhood of injection attacks here
+        sql = f"INSERT OR REPLACE INTO {table}({keys}) VALUES({values})"
+               
         try:
             with sqlite3.connect(self.db) as conn:
-                sql = f"INSERT OR REPLACE INTO \
-                    {table}({keys}) \
-                    VALUES({values})"
-                conn.executemany(sql, dictionary)
+                conn.executemany(sql, value_list)
                 print(f"{table} table updated successfully!")
                 
         except sqlite3.Error as error:
-            print(f"Error while updating a {table} table", error)
+            print(f"Error while updating a {table} table. ERROR:", error)
     
     
-    
+yt_sentiment().update_categories()
     

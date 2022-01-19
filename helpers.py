f3a9493c0f043ef3be5c06f325709e71c3d19d28
Nick Meares <nickmeares@gmail.com>
2022-01-19 10:57:21 +0000

    January 19, 2022 10:57 AM

--- helpers.py
+++ helpers.py
@@ -83,3 +83,7 @@ def paginated(max_pages):
                 page = 1
         return wrapper
     return decorate
+
+# Function to chunk list
+def chunked_list(lst:list, n:int):
+    return [lst[i:i + n] for i in range(0, len(lst), n)]

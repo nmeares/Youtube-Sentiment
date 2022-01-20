1fef8e62d9449eee2e6d6e03c32411ee49ffbbde
Nick <nickmeares@gmail.com>
2022-01-20 11:02:01 +0000

    January 20, 2022 11:02 AM

--- helpers.py
+++ helpers.py
@@ -71,10 +71,7 @@ def paginated(max_pages):
                     try:
                         # Run api function, increment page count and append next page token to kwargs
                         # Execute wrapped function
-                        try:
-                            response = func(*args, **kwargs)
-                        except Exception as e:
-                            print(e)
+                        response = func(*args, **kwargs)
                         combined.append(response)
                         page += 1
                         kwargs['pageToken'] = response['nextPageToken']

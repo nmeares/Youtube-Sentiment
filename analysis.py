d092a6e5f7200ac948b3f0bef284e57d34e4dd67
Nick Meares <nickmeares@gmail.com>
2022-01-21 18:26:00 +0000

    January 21, 2022 6:26 PM

--- analysis.py
+++ analysis.py
@@ -10,17 +10,31 @@ class sentiment():
         self.nlp = spacy.load('en_core_web_sm')
         self.doc = self.nlp(self.text)
         self.tokens = self._tokenise(self.doc)
-        self.filtered = self._dropwords()
+        self.filtered = self._dropwords(self.tokens)
+        self.polarity = self._polarity()
         
-    def _tokenise(doc: object):
+    def _tokenise(self, doc: object):
         token_list = []
         for token in doc:
             token_list.append(doc.text)
         return token_list
     
-    def _dropwords(tokenised_list: list):
+    def _dropwords(self, tokenised_list: list):
         filtered = []
         for word in tokenised_list:
-            lex = nlp.vocab[word]
+            lex = self.nlp.vocab[word]
             if lex.is_stop == False:
                 filtered.append(word)
+        return filtered
+    
+    def _polarity(self):
+        string = " ".join(self.filtered)
+        blob = TextBlob(string)
+        
+        if blob.sentiment.polarity > 0:
+            sentiment = 'positive'
+        elif blob.sentiment.polarity == 0:
+            sentiment = 'neutral'
+        elif blob.sentiment.polarity < 0:
+            sentiment = 'negative'
+        return sentiment

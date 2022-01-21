e6d11d41dd0434941c551f5a5db1454cb66e479b
Nick Meares <nickmeares@gmail.com>
2022-01-21 18:21:14 +0000

    January 21, 2022 6:21 PM

--- analysis.py
+++ analysis.py
@@ -9,11 +9,18 @@ class sentiment():
         self.text = text
         self.nlp = spacy.load('en_core_web_sm')
         self.doc = self.nlp(self.text)
-        self.tokens = self.tokenise(self.doc)
+        self.tokens = self._tokenise(self.doc)
+        self.filtered = self._dropwords()
         
-
-    def tokenise(doc: object):
+    def _tokenise(doc: object):
         token_list = []
         for token in doc:
             token_list.append(doc.text)
         return token_list
+    
+    def _dropwords(tokenised_list: list):
+        filtered = []
+        for word in tokenised_list:
+            lex = nlp.vocab[word]
+            if lex.is_stop == False:
+                filtered.append(word)

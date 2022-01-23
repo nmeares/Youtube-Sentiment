508962ac28093214e65f7cd067197c809918f43e
Nick <nickmeares@gmail.com>
2022-01-23 16:45:50 +0000

    January 23, 2022 4:45 PM

--- analysis.py
+++ analysis.py
@@ -3,7 +3,19 @@ import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from textblob import TextBlob
 import timeit
+from functools import wraps
 
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
 
 class sentiment():
     

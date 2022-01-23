f0bd2c46a52000f8d5e8d32c2db45fe36c2cf72e
Nick <nickmeares@gmail.com>
2022-01-23 18:13:03 +0000

    January 23, 2022 6:13 PM

--- testing.py
+++ testing.py
@@ -1,21 +1,22 @@
 import asyncio
 
-async def factorial(name, number):
-    f = 1
-    for i in range(2, number + 1):
-        print(f"Task {name}: Compute factorial({number}), currently i={i}...")
-        await asyncio.sleep(1)
-        f *= i
-    print(f"Task {name}: factorial({number}) = {f}")
-    return f
+class test():
+    async def factorial(name, number):
+        f = 1
+        for i in range(2, number + 1):
+            print(f"Task {name}: Compute factorial({number}), currently i={i}...")
+            await asyncio.sleep(1)
+            f *= i
+        print(f"Task {name}: factorial({number}) = {f}")
+        return f
 
-async def main():
-    # Schedule three calls *concurrently*:
-    L = await asyncio.gather(
-        factorial("A", 2),
-        factorial("B", 3),
-        factorial("C", 4),
-    )
-    print(L)
+    async def main():
+        # Schedule three calls *concurrently*:
+        L = await asyncio.gather(
+            test.factorial("A", 2),
+            test.factorial("B", 3),
+            test.factorial("C", 4),
+        )
+        print(L)
 
-asyncio.run(main())
+asyncio.run(test.main())

4b58bfdb514e9bc11e8dd777217fdfc04d5cf5ba
Nick <nickmeares@gmail.com>
2022-01-23 16:50:07 +0000

    January 23, 2022 4:50 PM

--- analysis.py
+++ analysis.py
@@ -60,5 +60,5 @@ class sentiment():
     
 def sent(text:list):
     sentiment_object = sentiment(text)
-    results = await asyncio.run(sentiment_object._async_polarity())
-    return results
+    results = sentiment_object._async_polarity()
+    return asyncio.run(results)
